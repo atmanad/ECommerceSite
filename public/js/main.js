b22cb32dd0dfe465bb87d702e4f6ec8fcbb73ace
atmanad <atmanad0001@gmail.com>
2020-08-19 14:19:59 +0530

    commit: 1
    index, mobiles, m01, signin/signup

---
+++ public/js/main.js
@@ -0,0 +1,43 @@
+// dropdowns
+//mobiles
+$('#mobiles').hover(() => {
+    $('[aria-labelledby="mobiles"]').fadeIn(400);
+});
+$('#mobiles').parent().on('mouseleave', () => {
+    $('[aria-labelledby="mobiles"]').fadeOut(400);
+});
+//appliances
+$('#appliances').hover(() => {
+    $('[aria-labelledby="appliances"]').fadeIn(400);
+});
+$('#appliances').parent().on('mouseleave', () => {
+    $('[aria-labelledby="appliances"]').fadeOut(400);
+});
+//men
+$('#men').hover(() => {
+    $('[aria-labelledby="men"]').fadeIn(400);
+});
+$('#men').parent().on('mouseleave', () => {
+    $('[aria-labelledby="men"]').fadeOut(400);
+});
+//women
+$('#women').hover(() => {
+    $('[aria-labelledby="women"]').fadeIn(400);
+});
+$('#women').parent().on('mouseleave', () => {
+    $('[aria-labelledby="women"]').fadeOut(400);
+});
+//books
+$('#books').hover(() => {
+    $('[aria-labelledby="books"]').fadeIn(400);
+});
+$('#books').parent().on('mouseleave', () => {
+    $('[aria-labelledby="books"]').fadeOut(400);
+});
+//furnitures
+$('#furnitures').hover(() => {
+    $('[aria-labelledby="furnitures"]').fadeIn(400);
+});
+$('#furnitures').parent().on('mouseleave', () => {
+    $('[aria-labelledby="furnitures"]').fadeOut(400);
+});
